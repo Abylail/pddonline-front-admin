84a17ac618ec9f9c10ba9644ee3d1522d3b4e85c
Абылай <bazarbayev.abylay@gmail.com>
2024-03-20 19:02:43 +0500

    refresh user token

--- store/auth/index.js
+++ store/auth/index.js
@@ -8,6 +8,7 @@ export const state = () => ({
   accessToken: null,
   refreshToken: null,
 
+  checkTimeout: null,
 })
 
 export const getters = {
@@ -31,6 +32,10 @@ export const getters = {
 export const mutations = {
   set(state, [namespace, value]) {
     state[namespace] = value;
+  },
+  setCheckTimeout(state, timeout) {
+    clearTimeout(state.checkTimeout);
+    state.checkTimeout = timeout;
   },
   setProperty(state, [namespace, propertyNamespace, value]) {
     let newObject = state[namespace] || {};
@@ -57,22 +62,52 @@ export const actions = {
   },
 
   // Логин через token
-  async tokenAuth({ commit, dispatch, getters }) {
-    if (!getters.getToken) {
-      commit("set", ["accessToken", this.$cookies.get("accessToken")]);
-      commit("set", ["refreshToken", this.$cookies.get("refreshToken")]);
-    }
+  tokenAuth({ commit, dispatch, getters, state }, refreshOnError = true) {
+    return new Promise(resolve => {
 
-    await this.$api.$get("/users/profile/")
-      .then(response => {
-        if (!response.err) {
-          let userInfo = null;
-          if (Array.isArray(response)) userInfo = response.find(u => u.role === rolesDict.admin.code)
-          else userInfo = response;
-          commit("set", ["userInfo", userInfo]);
-        }
-        else {
-          dispatch("logout");
+      if (!getters.getToken) {
+        commit("set", ["accessToken", this.$cookies.get("accessToken")]);
+        commit("set", ["refreshToken", this.$cookies.get("refreshToken")]);
+      }
+
+      if (!state.accessToken) resolve(false);
+
+      this.$api.$get("/users/profile/")
+        .then(async response => {
+          if (!response.err) {
+            // Успех
+            let userInfo = null;
+            if (Array.isArray(response)) userInfo = response.find(u => u.role === rolesDict.admin.code)
+            else userInfo = response;
+            commit("set", ["userInfo", userInfo]);
+            resolve(true);
+            commit("setCheckTimeout", setTimeout(() => dispatch("tokenAuth"), 10000));
+          }
+          else if (refreshOnError) {
+            await dispatch("refreshToken")
+            resolve(await dispatch("tokenAuth", false))
+          }
+          else {
+            // Ошибка
+            dispatch("logout");
+            resolve(false);
+          }
+        })
+
+    })
+  },
+
+  // Обновить токен
+  async refreshToken({state, commit}) {
+    const refresh = state.refreshToken || this.$cookies.get("refreshToken");
+    if (!refresh) return;
+    await this.$api.$post("/users/auth/refresh-token/", {refresh})
+      .then(({err, access, refresh}) => {
+        if (!err) {
+          commit("set", ["accessToken", access]);
+          commit("set", ["refreshToken", refresh]);
+          this.$cookies.set("accessToken", access);
+          this.$cookies.set("refreshToken", refresh);
         }
       })
   },
@@ -84,6 +119,7 @@ export const actions = {
     commit("set", ["refreshToken", null]);
     this.$cookies.remove("accessToken");
     this.$cookies.remove("refreshToken");
+    this.$router.push("/login")
   },
 
   // Информация пользователя
